a22de5b946ecd5e8928808fb4564e1dd79441066
IGR15 <151957251+IGR15@users.noreply.github.com>
2023-12-07 23:46:42 +0200

    Update Juranle.java
    didnt upload the full code

--- Libraryapp/src/libraryapp/Juranle.java
+++ Libraryapp/src/libraryapp/Juranle.java
@@ -4,16 +4,50 @@
  */
 package libraryapp;
 
+
 import java.util.Date;
 
-/**
- *
- * @author MSI
- */
-public class Juranle {
+public class Juranle extends Book{
+        private int ConferenceNo;
+        private String ConferenceName;
 
-    Juranle(int ConferenceNo, String ConferenceName, String title, BookAuthor get, String genre, int version, Date date) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        public Juranle(String title, BookAuthor author, String genre, int version, Date date) {
+            super(title, author, genre, version, date);
+        }
+
+
+
+
+        public Juranle(int ConferenceNo, String ConferenceName, String title, BookAuthor author, String genre, int version, Date date) {
+            super(title, author, genre, version, date);
+            this.ConferenceNo = ConferenceNo;
+            this.ConferenceName = ConferenceName;
+        }
+
+
+
+        public int getConferenceNo() {
+            return ConferenceNo;
+        }
+
+        public void setConferenceNo(int ConferenceNo) {
+            this.ConferenceNo = ConferenceNo;
+        }
+
+        public String getConferenceName() {
+            return ConferenceName;
+        }
+
+        public void setConferenceName(String ConferenceName) {
+            this.ConferenceName = ConferenceName;
+        }
+
+        @Override
+        public String toString() {
+            return "Journal{" + "ConferenceNo=" + ConferenceNo + ", ConferenceName=" + ConferenceName + '}';
+        }
+
+
+
     }
-    
-}
+
